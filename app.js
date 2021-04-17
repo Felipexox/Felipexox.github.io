57fa6980113fc79e3d009c90995fa66bfcf72ec1
FelipeArruda <felipe@oktagon.com.br>
2021-04-17 12:34:48 -0300

    change: Added grid to show content home

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ var conteudo = document.querySelector("#pokemon-div");
 var logout = document.querySelector("#logout");
 var botao = document.querySelector("#submitPokemon");
 var img = document.getElementById("imgPokemon");
+
+const gameIndex = document.querySelector("#game-index")
+const name = document.querySelector("#name")
+const type = document.querySelector("#type")
+const abilityName = document.querySelector("#ability-name")
+
+const pokemon_list = document.querySelector('#pokemon-list')
+
 botao.addEventListener("click",busca)
 
 if(!localStorage.getItem('token')){
@@ -10,29 +18,24 @@ if(!localStorage.getItem('token')){
   window.location.href = "./index.html";
 }
 
+listPokemons();
+
 logout.addEventListener('click', () => {
   localStorage.setItem('token', "");
   window.location.href = "./index.html";
 })
+
 
 function busca(event){
       event.preventDefault();
       var nomePokemon = nomePokemonCampo.value;
       axios.get(`https://pokeapi.co/api/v2/pokemon/${nomePokemon}`)
       .then(res => {
-        if(res.data.erro){
-          throw new Error('Nao existe esse pokemon')
-        }
-      
-        conteudo.innerHTML = ''
-        
-       
+        gameIndex.textContent = res.data.game_indices[3].game_index;
+        name.textContent = res.data.name;
+        type.textContent = res.data.types[0].type.name;
+        abilityName.textContent = res.data.abilities[0].ability.name;
 
-        
-        createLine(res.data.game_indices[3].game_index)
-        createLine(res.data.name)
-        createLine(res.data.types[0].type.name)
-        createLine(res.data.abilities[0].ability.name)
         img.setAttribute("src",(res.data.sprites.front_default))
        
       
@@ -44,11 +47,21 @@ function busca(event){
         createLine('ERRO!')
         img.setAttribute("#")
       })
-      
-      
-      
-
 }
+function listPokemons(){
+  
+  axios.get(`https://pokeapi.co/api/v2/pokemon?limit=20&offset=200`)
+  .then(res => {
+    showPokemonList(res.data.results)
+  })
+  .catch(err => {
+    conteudo.innerHTML = ''
+ 
+    createLine('ERRO!')
+    img.setAttribute("#")
+  })
+}
+
 
 function createLine(valor){
   let linha = document.createElement("p");
@@ -57,3 +70,17 @@ function createLine(valor){
   conteudo.appendChild(linha);
 }
 
+
+function showPokemonList(list){
+  while (pokemon_list.firstChild) {
+    pokemon_list.removeChild(pokemon_list.firstChild);
+  }
+
+  for(let i = 0; i < list.length; i++){
+    let pokemonElement = document.createElement("p");
+    pokemonElement.style.color = "#ffffff";
+    let pokemonChild = document.createTextNode(list[i].name);
+    pokemonElement.appendChild(pokemonChild);
+    pokemon_list.appendChild(pokemonElement);
+  }
+}
