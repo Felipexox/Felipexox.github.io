284a8498cc121e474dc3ac9203d04bd19b3c2b51
headleo <leohsouza12@hotmail.com>
2021-05-21 23:29:27 -0300

    implantações trabalho 3 parte 2

--- login.js
+++ login.js
@@ -15,13 +15,14 @@ sign_in_btn.addEventListener('click', () => {
   
   if(email.value.length > 3 && password.value.length > 3 && validateEmail(email.value)){
 
-    axios.post(`https://twitter-express-backend.herokuapp.com/login`,  
-     JSON.stringify({
+    axios.post(`https://twitter-express-backend.herokuapp.com/login`, 
+     {
         email: email.value,
         password: password.value
-     })
+     }
     )
       .then(res => {
+        console.log(JSON.stringify(res.data))
         if(res.data.token){
           localStorage.setItem('token', res.data.token)
           window.location.href = "./home.html";
